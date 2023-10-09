4aae0606b2e9a2ba2166b31a5804ec97664d49a3
jmulholland4014 <62855716+jmulholland4014@users.noreply.github.com>
2023-10-09 11:46:37 -0400

    Sword does damage

--- Characters/Allys/player.gd
+++ Characters/Allys/player.gd
@@ -7,11 +7,14 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sword = get_node("Sword")
+@onready var sword_animation_player = sword.get_node("SwordAnimationPlayer")
 
 func _physics_process(delta):
 	var mouse_direction = (get_global_mouse_position() - global_position).normalized()
 	if (sword.scale.y > 0 and mouse_direction.x < 0) or (sword.scale.y < 0 and mouse_direction.x > 0):
 		sword.scale.y *= -1
+	if Input.is_action_just_pressed("left_click") and not sword_animation_player.is_playing():
+		sword_animation_player.play("attack")
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -33,3 +36,8 @@ func _physics_process(delta):
 	sword.rotation = mouse_direction.angle()
 
 	move_and_slide()
+
+
+func _on_hit_box_body_entered(body):
+	body.take_damage(20)
+	pass # Replace with function body.
